94f9097c255f08dbb2f05a2b335f6c54ca4205e7
woulvee <gianlucadellaquila09@gmail.com>
2025-11-23 22:15:02 +0100

    Rebranded as Surventure
    Removed Trident Recipes and added a new recipe
    Removed Vanilla Lead Recipe
    Nerfed Wildernature Ammunition
    Changed Thalassoger Loot Table

---
+++ src/main/java/net/woulvee/surventure/Config.java
@@ -0,0 +1,18 @@
+package net.woulvee.surventure;
+
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.neoforged.neoforge.common.ModConfigSpec;
+
+// An example config class. This is not required, but it's a good idea to have one to keep your config organized.
+// Demonstrates how to use Neo's config APIs
+public class Config {
+    private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
+
+
+    static final ModConfigSpec SPEC = BUILDER.build();
+
+    private static boolean validateItemName(final Object obj) {
+        return obj instanceof String itemName && BuiltInRegistries.ITEM.containsKey(ResourceLocation.parse(itemName));
+    }
+}
---
+++ src/main/java/net/woulvee/surventure/item/ModItems.java
@@ -0,0 +1,27 @@
+package net.woulvee.surventure.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Rarity;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.woulvee.surventure.QuestItems;
+
+public class ModItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(QuestItems.MODID);
+
+    public static final DeferredItem<Item> NETHERPORTAL = ITEMS.register("mini_nether_portal",
+            () -> new Item(new Item.Properties().stacksTo(1)));
+    public static final DeferredItem<Item> ENDPORTAL = ITEMS.register("mini_end_portal",
+            () -> new Item(new Item.Properties().stacksTo(1)));
+    public static final DeferredItem<Item> DEEPDARK = ITEMS.register("mini_deepdark_portal",
+            () -> new Item(new Item.Properties().stacksTo(1)));
+    public static final DeferredItem<Item> TWILIGHT = ITEMS.register("mini_twilight_portal",
+            () -> new Item(new Item.Properties().stacksTo(1)));
+    public static final DeferredItem<Item> TWILIGHT_CRYSTAL = ITEMS.register("twilight_crystal",
+            () -> new Item(new Item.Properties().stacksTo(1).rarity(Rarity.RARE)));
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+}
